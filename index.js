900972b4de77812812a0a04f125c38f19e6e1058
kevinanton01 <kevinanton518@gmail.com>
2026-06-05 11:35:32 -0300

    Mejora en el inicio de secion

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-/*En caso que no se haya ingresado nombre de usuario no se podra acceder */
+/* En caso que no se haya ingresado nombre de usuario no se podra acceder */
 function ingresar() {
-    const inputNombre = document.getElementById("nombre");
-    const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("nombre");
 
-    if (nombre === "") {
-        inputNombre.style.border = "2px solid red";
+    if (nombre.value === "") {
+        nombre.style.border = "2px solid red";
         return;
     }
 
-    window.location.href = "./pages/cliente/cliente.html";
+    location.href = "./pages/cliente/cliente.html";
+}
+
+/* Se muestra el nuevo bloque en el que se puede ingresar como administrador */
+function mostrarLoginAdmin() {
+    document.getElementById("loginAdmin").style.display = "block";
+}
+
+/* En caso de no ser admin, se puede cancelar y volver al login original */
+function cerrarLoginAdmin() {
+    document.getElementById("loginAdmin").style.display = "none";
+}
+
+function validarAdmin() {
+    const usuario = document.getElementById("usuarioAdmin");
+    const password = document.getElementById("passwordAdmin");
+
+    if (usuario.value === "admin" && password.value === "1234") {
+        location.href = "./pages/admin/admin.html";
+    } else {
+        usuario.style.border = "2px solid red";
+        password.style.border = "2px solid red";
+        alert("Usuario o contraseña incorrectos");
+    }
 }
