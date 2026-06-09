b850a7fd897040c85b36fbd49ef7a22d7bd62e21
GabrielPabon616 <gabriel_pabon@hotmail.com>
2026-06-08 22:13:52 -0300

    creacion de la pagina de clientes y el carrito

---
+++ pages/carrito/carrito.js
@@ -0,0 +1,130 @@
+let arrayProteinas=JSON.parse(localStorage.getItem("proteinas"));
+let arrayShackers=JSON.parse(localStorage.getItem("shackers"));
+let arrayCreatinas=JSON.parse(localStorage.getItem("creatinas"));
+//let arrayCarrito=[];
+//if(localStorage.getItem("Carrito")!==null){
+//    arrayCarrito=JSON.parse(localStorage.getItem("Carrito"));
+//}
+
+let arrayCarritoUnico=[];
+
+function crearCarrito(){
+    let zonaCarrito=document.getElementById("carrito-productos");
+    let stringCarritoExtraido=localStorage.getItem("Carrito");
+    let arrayCarritoExtraido=JSON.parse(stringCarritoExtraido);
+    
+    arrayCreatinas.forEach(creatina=>{
+        if(arrayCarritoExtraido.some(producto=>producto.nombre===creatina.nombre)===true){
+            arrayCarritoUnico.push(creatina);
+        }
+    });
+    arrayShackers.forEach(shacker=>{
+        if(arrayCarritoExtraido.some(producto=>producto.nombre===shacker.nombre)===true){
+            arrayCarritoUnico.push(shacker);
+        }
+    });
+    arrayProteinas.forEach(proteina=>{
+        if(arrayCarritoExtraido.some(producto=>producto.nombre===proteina.nombre)===true){
+            arrayCarritoUnico.push(proteina);
+        }
+    });
+
+    arrayCarritoUnico.forEach(productounico=>{
+
+        let arrayProductos=arrayCarritoExtraido.filter(productoextraido=>productoextraido.nombre===productounico.nombre);
+        let cantidad=arrayProductos.length;
+        productounico.cantidad=cantidad;
+    })
+
+    let arrayCarritoUnicoHTML=arrayCarritoUnico.map(producto=>`<div><div class="contenedor-imagen"><img  src="${producto.img}" alt="${producto.nombre}" class="imagen-carrito" ></div><p>${producto.nombre}</p>${producto.precio}<p></p><button class="boton-agregar" id="boton-agregar-${producto.nombre}">+</button><p id="cantidad-${producto.nombre}">${producto.cantidad}</p><button class="boton-borrar" id="boton-borrar-${producto.nombre}">-</button></div><hr>`);
+
+    
+    let stringCarritoUnicoHTML=arrayCarritoUnicoHTML.join("");
+    const total=arrayCarritoUnico.reduce((acumulador,elemento)=>acumulador + elemento.precio*elemento.cantidad,0);
+    stringCarritoUnicoHTML +=`<p>TOTAL:${total}</p>`;
+    zonaCarrito.innerHTML=stringCarritoUnicoHTML;
+
+}
+
+function crearEscuchadorBotonAgregar(texto){
+    //arrayCarrito=JSON.parse(localStorage.getItem("Carrito"));
+    let zonaCantidad=document.getElementById("cantidad-"+texto);
+    let boton=document.getElementById(`boton-agregar-${texto}`);
+    boton.addEventListener("click",()=>{
+        let arrayCarrito=JSON.parse(localStorage.getItem("Carrito"));
+        arrayProteinas.forEach(proteina=>{
+            if(proteina.nombre===texto){
+                arrayCarrito.push(structuredClone(proteina));
+            }
+        });
+        arrayCreatinas.forEach(creatina=>{
+            if(creatina.nombre===texto){
+                arrayCarrito.push(structuredClone(creatina));
+            }
+        });
+        arrayShackers.forEach(shacker=>{
+            if(shacker.nombre===texto){
+                arrayCarrito.push(structuredClone(shacker));
+            }
+        });
+        let CarritoJSON=JSON.stringify(arrayCarrito);
+        localStorage.setItem("Carrito",CarritoJSON);
+
+
+
+        let posicion;
+        arrayCarritoUnico.forEach((producto,indice)=>{
+            if(producto.nombre===texto){
+                producto.cantidad += 1;
+                posicion=producto.cantidad;
+            }
+        });
+
+        zonaCantidad.innerHTML=posicion;
+    });
+
+}
+
+function crearEscuchadorBotonBorrar(texto){
+    let zonaCantidad=document.getElementById("cantidad-"+texto);
+    let boton=document.getElementById("boton-borrar-"+texto);
+    boton.addEventListener("click",()=>{
+        let JSONCarrito=localStorage.getItem("Carrito");
+        if(JSONCarrito!==null){
+            let arrayCarritoBorrar=JSON.parse(JSONCarrito);
+            for (let i = 0; i < arrayCarritoBorrar.length; i++) {
+                if(arrayCarritoBorrar[i].nombre===texto){
+                    arrayCarritoBorrar.splice(i,1);
+                    break;
+                }
+                
+            }
+            
+            localStorage.setItem("Carrito",JSON.stringify(arrayCarritoBorrar));
+
+        }
+        
+
+
+        let posicion=0;
+        arrayCarritoUnico.forEach((producto,indice)=>{
+            if(producto.nombre===texto && producto.cantidad>=1){
+                producto.cantidad -= 1;
+                posicion=producto.cantidad;
+            }
+        });
+
+        zonaCantidad.innerHTML=posicion;
+    
+    
+    
+    });
+
+}
+console.table(arrayCreatinas);
+console.table(arrayProteinas);
+console.table(arrayShackers);
+crearCarrito();
+arrayCarritoUnico.forEach(producto=>crearEscuchadorBotonAgregar(producto.nombre));
+
+arrayCarritoUnico.forEach(producto=>crearEscuchadorBotonBorrar(producto.nombre));
---
+++ pages/cliente/cliente.js
@@ -0,0 +1,73 @@
+let proteinas = [{ 'id': 1, 'nombre': 'Isoprot', 'precio': 126000, 'img': '../../img/Isoprot.webp' }, { 'id': 2, 'nombre': 'Ultra-mass', 'precio': 59400, 'img': '../../img/Ultra-mass.webp' }, { 'id': 3, 'nombre': 'Whey-x-pro', 'precio': 98000, 'img': '../../img/Whey-x-pro.webp' }, { 'id': 4, 'nombre': 'Starter-protein', 'precio': 42300, 'img': '../../img/Starter-protein.webp' }, { 'id': 5, 'nombre': 'Plant-protein', 'precio': 53000, 'img': '../../img/Plant-protein.webp' }];
+let creatinas = [{ 'id': 6, 'nombre': 'Creatina-monohidrato', 'precio': 45000, 'img': '../../img/Creatina-monohidrato.webp' }, { 'id': 7, 'nombre': 'Creatina-electrolitos', 'precio': 31200, 'img': '../../img/Creatina-electrolitos.webp' }, { 'id': 8, 'nombre': 'Creatina-monohidrato-1kilo', 'precio': 92000, 'img': '../../img/Creatina-monohidrato-1kilo.webp' }];
+let shackers = [{ 'id': 9, 'nombre': 'Shaker-ENA', 'precio': 6700, 'img': '../../img/Shaker-ENA.webp' }, { 'id': 10, 'nombre': 'Shaker-PLUS', 'precio': 7400, 'img': '../../img/Shaker-PLUS.webp' }, { 'id': 11, 'nombre': 'Shaker-premium-truemade', 'precio': 27000, 'img': '../../img/Shaker-premium-truemade.webp' }, { 'id': 12, 'nombre': 'Shaker-premium', 'precio': 27000, 'img': '../../img/Shaker-premium.webp' }];
+
+
+let arrayCarrito = [];
+
+localStorage.setItem("proteinas", JSON.stringify(proteinas));
+localStorage.setItem("creatinas", JSON.stringify(creatinas));
+localStorage.setItem("shackers", JSON.stringify(shackers));
+
+function insertarProductos(array, id) {
+    const zona = document.getElementById(id);
+    let arrayzona = array.map(producto => `<div class="productos"><div class="contenedor-imagen"><img src="${producto.img}" alt="${producto.nombre}" class="img-producto"></div><p class="nombre-producto">${producto.nombre}</p><p class="precio-producto">$${producto.precio}</p><div class="botones-productos"><button class="boton-agregar" id="boton-agregar-${producto.nombre}">+</button><button class="boton-borrar" id="boton-borrar-${producto.nombre}">-</button></div></div>`);
+    const stringZona = arrayzona.join("");
+    zona.innerHTML = stringZona;
+}
+
+insertarProductos(proteinas, "listado-proteinas");
+insertarProductos(creatinas, "listado-creatinas");
+insertarProductos(shackers, "listado-shackers");
+
+function crearEscuchadorBotonAgregar(texto) {
+    let boton = document.getElementById(`boton-agregar-${texto}`);
+    boton.addEventListener("click", () => {
+        proteinas.forEach(proteina => {
+            if (proteina.nombre === texto) {
+                arrayCarrito.push(structuredClone(proteina));
+            }
+        });
+        creatinas.forEach(creatina => {
+            if (creatina.nombre === texto) {
+                arrayCarrito.push(structuredClone(creatina));
+            }
+        });
+        shackers.forEach(shacker => {
+            if (shacker.nombre === texto) {
+                arrayCarrito.push(structuredClone(shacker));
+            }
+        });
+        let CarritoJSON = JSON.stringify(arrayCarrito);
+        localStorage.setItem("Carrito", CarritoJSON);
+    });
+
+}
+
+function crearEscuchadorBotonBorrar(texto) {
+    let boton = document.getElementById("boton-borrar-" + texto);
+    boton.addEventListener("click", () => {
+        let JSONCarrito = localStorage.getItem("Carrito");
+        if (JSONCarrito !== null) {
+            let arrayCarritoBorrar = JSON.parse(JSONCarrito);
+            for (let i = 0; i < arrayCarritoBorrar.length; i++) {
+                if (arrayCarritoBorrar[i].nombre === texto) {
+                    arrayCarritoBorrar.splice(i, 1);
+                    break;
+                }
+
+            }
+
+            localStorage.setItem("Carrito", JSON.stringify(arrayCarritoBorrar));
+
+        }
+    })
+
+}
+proteinas.forEach(proteina => crearEscuchadorBotonAgregar(proteina.nombre));
+shackers.forEach(shacker => crearEscuchadorBotonAgregar(shacker.nombre));
+creatinas.forEach(creatina => crearEscuchadorBotonAgregar(creatina.nombre));
+
+proteinas.forEach(proteina => crearEscuchadorBotonBorrar(proteina.nombre));
+shackers.forEach(shacker => crearEscuchadorBotonBorrar(shacker.nombre));
+creatinas.forEach(creatina => crearEscuchadorBotonBorrar(creatina.nombre));
